dabcb577abf3d20d89db48eae03fa8604c33118a
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-04-16 20:34:21 +0200

    Format Terraform files

--- beanstalk.tf
+++ beanstalk.tf
@@ -1,75 +1,75 @@
 resource "aws_elastic_beanstalk_application" "cloudtictactoe_app" {
-  name = "Cloud Tic Tac Toe"
+  name        = "Cloud Tic Tac Toe"
   description = "Cloud Tic Tac Toe Application"
 }
 
 resource "aws_elastic_beanstalk_application_version" "cloudtictactoe_app_version" {
-  name = "v1.0.0"
+  name        = "v1.0.0"
   description = "Cloud Tic Tac Toe Application Version 1.0.0"
   application = aws_elastic_beanstalk_application.cloudtictactoe_app.name
 
   bucket = aws_s3_bucket.cloudtictactoe_server_bucket.id
-  key = aws_s3_object.cloudtictactoe_server_source_bundle.id
+  key    = aws_s3_object.cloudtictactoe_server_source_bundle.id
 }
 
 resource "aws_elastic_beanstalk_environment" "cloudtictactoe_env" {
-  name = "cloudtictactoe-env"
-  application = aws_elastic_beanstalk_application.cloudtictactoe_app.name
-  version_label = aws_elastic_beanstalk_application_version.cloudtictactoe_app_version.name
+  name                = "cloudtictactoe-env"
+  application         = aws_elastic_beanstalk_application.cloudtictactoe_app.name
+  version_label       = aws_elastic_beanstalk_application_version.cloudtictactoe_app_version.name
   solution_stack_name = "64bit Amazon Linux 2 v3.8.0 running Docker"
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
-    name = "IamInstanceProfile"
-    value = "LabInstanceProfile"
+    name      = "IamInstanceProfile"
+    value     = "LabInstanceProfile"
   }
 
   setting {
     namespace = "aws:ec2:vpc"
-    name = "VPCId"
-    value = aws_vpc.cloudtictactoe_server_vpc.id
+    name      = "VPCId"
+    value     = aws_vpc.cloudtictactoe_server_vpc.id
   }
 
   setting {
     namespace = "aws:ec2:vpc"
-    name = "Subnets"
-    value = aws_subnet.cloudtictactoe_server_subnet1.id
+    name      = "Subnets"
+    value     = aws_subnet.cloudtictactoe_server_subnet1.id
   }
 
   setting {
     namespace = "aws:ec2:vpc"
-    name = "ELBSubnets"
-    value = aws_subnet.cloudtictactoe_server_subnet1.id
+    name      = "ELBSubnets"
+    value     = aws_subnet.cloudtictactoe_server_subnet1.id
   }
 
   setting {
     namespace = "aws:ec2:vpc"
-    name = "ELBScheme"
-    value = "internetFacing"
+    name      = "ELBScheme"
+    value     = "internetFacing"
   }
 
   setting {
     namespace = "aws:elbv2:loadbalancer"
-    name = "SecurityGroups"
-    value = "${aws_security_group.cloudtictactoe_server_sg_http.id},${aws_security_group.cloudtictactoe_server_sg_ssh.id}"
+    name      = "SecurityGroups"
+    value     = "${aws_security_group.cloudtictactoe_server_sg_http.id},${aws_security_group.cloudtictactoe_server_sg_ssh.id}"
   }
 
   setting {
     namespace = "aws:elb:listener:80"
-    name = "ListenerProtocol"
-    value = "TCP"
+    name      = "ListenerProtocol"
+    value     = "TCP"
   }
 
   setting {
     namespace = "aws:elb:listener:80"
-    name = "InstanceProtocol"
-    value = "TCP"
+    name      = "InstanceProtocol"
+    value     = "TCP"
   }
 
   setting {
     namespace = "aws:ec2:vpc"
-    name = "AssociatePublicIpAddress"
-    value = "true"
+    name      = "AssociatePublicIpAddress"
+    value     = "true"
   }
 
   tags = {
--- s3.tf
+++ s3.tf
@@ -1,10 +1,10 @@
 data "external" "create_source_bundle" {
-    program = [ "bash", "${path.module}/make-source-bundle.sh"]
+  program = ["bash", "${path.module}/make-source-bundle.sh"]
 }
 
 resource "aws_s3_bucket" "cloudtictactoe_server_bucket" {
   bucket = "cloudtictactoe-bucket-1"
-  
+
   tags = {
     Name = "Cloud Tic Tac Toe S3 Bucket"
   }
@@ -12,11 +12,11 @@ resource "aws_s3_bucket" "cloudtictactoe_server_bucket" {
 
 resource "aws_s3_object" "cloudtictactoe_server_source_bundle" {
   bucket = aws_s3_bucket.cloudtictactoe_server_bucket.id
-  key = data.external.create_source_bundle.result.path
+  key    = data.external.create_source_bundle.result.path
   source = data.external.create_source_bundle.result.path
-  etag = data.external.create_source_bundle.result.md5
+  etag   = data.external.create_source_bundle.result.md5
 
-  depends_on = [ data.external.create_source_bundle ]
+  depends_on = [data.external.create_source_bundle]
 
   tags = {
     Name = "Cloud Tic Tac Toe Source Bundle"
