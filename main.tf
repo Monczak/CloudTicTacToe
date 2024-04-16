189376a1c1c23c8cf287045b4506fee8c9bd367a
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-04-16 16:40:42 +0200

    Refactor main.tf

--- main.tf
+++ main.tf
@@ -84,9 +84,6 @@ resource "aws_ecs_service" "cloudtictactoe_service" {
   desired_count = 1
   launch_type = "FARGATE"
 
-  deployment_minimum_healthy_percent = 0
-  deployment_maximum_percent = 100
-
   network_configuration {
     assign_public_ip = true
     security_groups = [ aws_security_group.cloudtictactoe_server_sg_http.id ]
