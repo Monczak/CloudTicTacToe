1c45fdfbb5f51608ecedb156cc285bfeaa75c25f
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-04-16 20:29:25 +0200

    Streamline source bundle creation

--- make-source-bundle.sh
+++ make-source-bundle.sh
@@ -2,5 +2,6 @@
 
 SOURCE_BUNDLE_PATH=source-bundle.zip
 
+rm "$SOURCE_BUNDLE_PATH"
 zip -r "$SOURCE_BUNDLE_PATH" backend frontend nginx docker-compose.yml > /dev/null
-echo "{\"md5\":\"$(md5sum "$SOURCE_BUNDLE_PATH" | awk '{ print $1 }')\"}"
+echo "{\"path\":\"$SOURCE_BUNDLE_PATH\",\"md5\":\"$(md5sum "$SOURCE_BUNDLE_PATH" | awk '{ print $1 }')\"}"
--- s3.tf
+++ s3.tf
@@ -1,10 +1,5 @@
-variable "source_bundle" {
-    type = string
-    default = "source-bundle.zip"
-}
-
 data "external" "create_source_bundle" {
-    program = [ "bash", "${path.module}/make-source-bundle.sh" ]
+    program = [ "bash", "${path.module}/make-source-bundle.sh"]
 }
 
 resource "aws_s3_bucket" "cloudtictactoe_server_bucket" {
@@ -17,8 +12,8 @@ resource "aws_s3_bucket" "cloudtictactoe_server_bucket" {
 
 resource "aws_s3_object" "cloudtictactoe_server_source_bundle" {
   bucket = aws_s3_bucket.cloudtictactoe_server_bucket.id
-  key = var.source_bundle
-  source = var.source_bundle
+  key = data.external.create_source_bundle.result.path
+  source = data.external.create_source_bundle.result.path
   etag = data.external.create_source_bundle.result.md5
 
   depends_on = [ data.external.create_source_bundle ]
