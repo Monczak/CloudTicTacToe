33ab8d6f7ac61985e8daf6469bc83b81564ea6f7
Monczak <szymon.jedrzejczak.sj@gmail.com>
2024-04-16 20:41:56 +0200

    Format Terraform files

--- fargate.tf
+++ fargate.tf
@@ -18,9 +18,9 @@ variable "aws_ecs_log_configuration" {
   default = {
     logDriver = "awslogs"
     options = {
-      "awslogs-create-group" = "true"
-      "awslogs-group" = "fargate-logs"
-      "awslogs-region" = "us-east-1"
+      "awslogs-create-group"  = "true"
+      "awslogs-group"         = "fargate-logs"
+      "awslogs-region"        = "us-east-1"
       "awslogs-stream-prefix" = "ecs"
     }
   }
@@ -30,64 +30,64 @@ resource "aws_ecs_task_definition" "cloudtictactoe_task" {
   family = "cloudtictactoe-task"
   container_definitions = jsonencode([
     {
-      name = "cloudtictactoe-frontend"
+      name  = "cloudtictactoe-frontend"
       image = "docker.io/monczak/cloudtictactoe-frontend-fargate:latest"
       portMappings = [
         {
           containerPort = 8000
-          hostPort = 8000
+          hostPort      = 8000
         }
       ]
       logConfiguration = var.aws_ecs_log_configuration
     },
     {
-      name = "cloudtictactoe-backend"
+      name  = "cloudtictactoe-backend"
       image = "docker.io/monczak/cloudtictactoe-backend-fargate:latest"
       portMappings = [
         {
           containerPort = 8001
-          hostPort = 8001
+          hostPort      = 8001
         }
       ]
       logConfiguration = var.aws_ecs_log_configuration
     },
     {
-      name = "cloudtictactoe-web"
+      name  = "cloudtictactoe-web"
       image = "docker.io/monczak/cloudtictactoe-web-fargate:latest"
       portMappings = [
         {
           containerPort = 80
-          hostPort = 80
+          hostPort      = 80
         }
       ]
       logConfiguration = var.aws_ecs_log_configuration
     },
   ])
 
-  network_mode = "awsvpc"
-  requires_compatibilities = [ "FARGATE" ]
-  cpu = "256"
-  memory = "0.5GB"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "0.5GB"
 
   execution_role_arn = "arn:aws:iam::854270513909:role/LabRole"
 
   tags = {
-    Name = "Cloud Tic Tac Toe Task Definition"
+    Name        = "Cloud Tic Tac Toe Task Definition"
     Description = "Sets up the 3 Docker containers needed to run Cloud Tic Tac Toe."
   }
 }
 
 resource "aws_ecs_service" "cloudtictactoe_service" {
-  name = "cloudtictactoe-service"
-  cluster = aws_ecs_cluster.cloudtictactoe_cluster.id
+  name            = "cloudtictactoe-service"
+  cluster         = aws_ecs_cluster.cloudtictactoe_cluster.id
   task_definition = aws_ecs_task_definition.cloudtictactoe_task.arn
-  desired_count = 1
-  launch_type = "FARGATE"
+  desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
     assign_public_ip = true
-    security_groups = [ aws_security_group.cloudtictactoe_server_sg_http.id ]
-    subnets = [ aws_subnet.cloudtictactoe_server_subnet1.id ]
+    security_groups  = [aws_security_group.cloudtictactoe_server_sg_http.id]
+    subnets          = [aws_subnet.cloudtictactoe_server_subnet1.id]
   }
 
   tags = {
